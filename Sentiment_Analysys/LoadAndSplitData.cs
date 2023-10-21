42ceee4795286ebfdd455a3bf22e7dbda7b7ccff
Slimcent <centpresley@gmail.com>
2023-10-21 18:28:00 +0100

    Created the load and split class.

---
+++ Sentiment_Analysys/LoadAndSplitData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ML;
+using Sentiment_Analysis.Models;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace Sentiment_Analysis
+{
+    public static class LoadAndSplitData
+    {
+        public static TrainTestData LoadData(MLContext mlContext)
+        {
+            string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            return splitDataView;
+        }
+    }
+}
