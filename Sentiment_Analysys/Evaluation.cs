9c95fae8ee458dd463405aedfa161bd86987131a
Slimcent <centpresley@gmail.com>
2023-10-21 19:24:30 +0100

    Evaluate the model.

---
+++ Sentiment_Analysys/Evaluation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ML.Data;
+using Microsoft.ML;
+
+namespace Sentiment_Analysis
+{
+    public static class Evaluation
+    {
+        public static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            IDataView predictions = model.Transform(splitTestSet);
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+        }
+    }
+}
