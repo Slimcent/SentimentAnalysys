7c4e201234d0f299bbd683efd2eb165cf9c1312f
Slimcent <centpresley@gmail.com>
2023-10-22 00:10:35 +0100

    Created class for model prediction in order to test our model.

---
+++ Sentiment_Analysys/ModelPrediction.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.ML;
+using Sentiment_Analysis.Models;
+
+namespace Sentiment_Analysis
+{
+    public static class ModelPrediction
+    {
+        public static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
+        {
+            PredictionEngine<SentimentData, SentimentPrediction> predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+
+            SentimentData sampleStatement = new()
+            {
+                SentimentText = "This was a very bad steak"
+            };
+
+            var resultPrediction = predictionFunction.Predict(sampleStatement);
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
+
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
+        }
+
+        public static void UseModelWithBatchItems(MLContext mlContext, ITransformer model)
+        {
+            IEnumerable<SentimentData> sentiments = new[]
+            {
+                new SentimentData
+                {
+                    SentimentText = "This was a horrible meal"
+                },
+                new SentimentData
+                {
+                    SentimentText = "I love this spaghetti."
+                }
+            };
+
+            IDataView batchComments = mlContext.Data.LoadFromEnumerable(sentiments);
+
+            IDataView predictions = model.Transform(batchComments);
+
+            // Use model to predict whether comment data is Positive (1) or Negative (0).
+            IEnumerable<SentimentPrediction> predictedResults = mlContext.Data.CreateEnumerable<SentimentPrediction>(predictions, reuseRowObject: false);
+
+            Console.WriteLine();
+
+            Console.WriteLine("=============== Prediction Test of loaded model with multiple samples ===============");
+
+            foreach (SentimentPrediction prediction in predictedResults)
+            {
+                Console.WriteLine($"Sentiment: {prediction.SentimentText} | Prediction: {(Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative")} | Probability: {prediction.Probability} ");
+            }
+            Console.WriteLine("=============== End of batch predictions ===============");
+        }
+    }
+}
