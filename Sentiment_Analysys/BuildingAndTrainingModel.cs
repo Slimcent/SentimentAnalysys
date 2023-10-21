d649e048e151d38719c469b6e6a4da65b1990a23
Slimcent <centpresley@gmail.com>
2023-10-21 18:38:35 +0100

    Created the bulding and training model class.

---
+++ Sentiment_Analysys/BuildingAndTrainingModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ML;
+using Sentiment_Analysis.Models;
+
+namespace Sentiment_Analysis
+{
+    public static class BuildingAndTrainingModel
+    {
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+
+            Console.WriteLine("=============== Create and Train the Model ===============\n");
+            var model = estimator.Fit(splitTrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+
+            return model;
+        }
+    }
+}
