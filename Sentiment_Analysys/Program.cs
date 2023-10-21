a633a66544d58bf590b80dbbc014896c6348a6dc
Slimcent <centpresley@gmail.com>
2023-10-22 00:11:17 +0100

    Sentiment analysis

--- Sentiment_Analysys/Program.cs
+++ Sentiment_Analysys/Program.cs
@@ -1,4 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Sentiment_Analysis;
+using Sentiment_Analysis.Models;
+using System;
+using static Microsoft.ML.DataOperationsCatalog;
+
+MLContext mlContext = new();
+
+// Load and split the data
+TrainTestData splitDataView = LoadAndSplitData.LoadData(mlContext);
+
+// Build and train the model
+ITransformer model = BuildingAndTrainingModel.BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+
+// Displaying the metrics for model validation
+Evaluation.Evaluate(mlContext, model, splitDataView.TestSet);
+
+// Predict the test data outcome on a single item
+ModelPrediction.UseModelWithSingleItem(mlContext, model);
+
+// Predict the test data outcome on many items
+ModelPrediction.UseModelWithBatchItems(mlContext, model);
 
 
